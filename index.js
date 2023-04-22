49b0058daf4d49c00fc359b6207c83013185f837
Krystian <krystian.potaczek07@gmail.com>
2023-04-22 22:16:59 +0200

    Finishing Overview page

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from "./data/index.js";
 
 dotenv.config();
@@ -53,5 +55,6 @@ mongoose
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProduct);
     // Transaction.insertMany(dataTransaction);
+    // OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
